a185f8897e546193fa7b30b723d2ec3b900958a8
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-02 17:27:47 +0300

    implemented coordinates data class

---
+++ common/src/main/java/com/megateam/common/data/Coordinates.java
@@ -0,0 +1,44 @@
+package com.megateam.common.data;
+
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * This data class contains coordinates information
+ * Is used in Ticket data class
+ */
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
+@XmlRootElement(name = "coordinates")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Coordinates
+{
+	/**
+	 * This field contains X coordinate
+	 */
+	@XmlElement(name = "xCoord", required = true)
+	private float x;
+
+	/**
+	 * This field contains Y coordinate
+	 */
+	@NonNull
+	@XmlElement(name = "yCoord", required = true)
+	private Integer y;
+
+	/**
+	 * This method provides accessing a string representation of coordinates class object
+	 * @return coordinates string representation
+	 */
+	@Override
+	public String toString()
+	{
+		return String.format("Coordinates: [x: %f, y: %d", x, y);
+	}
+}
