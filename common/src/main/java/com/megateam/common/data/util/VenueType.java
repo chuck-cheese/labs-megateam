285b88a194595c9d868aea2b2deb952f3783265d
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-02 17:31:52 +0300

    implemented venue type selector

---
+++ common/src/main/java/com/megateam/common/data/util/VenueType.java
@@ -0,0 +1,22 @@
+package com.megateam.common.data.util;
+
+/**
+ * Allowed venue types
+ */
+public enum VenueType
+{
+	/**
+	 * Bar venue type
+	 */
+	BAR,
+
+	/**
+	 * Loft venue type
+	 */
+	LOFT,
+
+	/**
+	 * Mall venue type
+	 */
+	MALL;
+}
