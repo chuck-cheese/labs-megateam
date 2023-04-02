5ff143e224aeed327aa2f001262fcfe144e4b60e
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-03 01:59:16 +0300

    implemented ticket type selector

---
+++ common/src/main/java/com/megateam/common/data/util/TicketType.java
@@ -0,0 +1,22 @@
+package com.megateam.common.data.util;
+
+/**
+ * Allowed ticket types
+ */
+public enum TicketType
+{
+	/**
+	 * Vip ticket type
+	 */
+	VIP,
+
+	/**
+	 * Usual ticket type
+	 */
+	USUAL,
+
+	/**
+	 * Budgetary ticket type
+	 */
+	BUDGETARY;
+}
