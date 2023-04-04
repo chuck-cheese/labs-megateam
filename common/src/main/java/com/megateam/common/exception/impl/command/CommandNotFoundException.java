bd5503df77bbefa3eefb61d1ad0cc41c5059b3c8
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 01:39:47 +0300

    implemented default command execution method using exception & command not found exception

---
+++ common/src/main/java/com/megateam/common/exception/impl/command/CommandNotFoundException.java
@@ -0,0 +1,19 @@
+package com.megateam.common.exception.impl.command;
+
+import com.megateam.common.exception.CommandException;
+
+/**
+ * This exception is thrown if command not found during resolving
+ */
+public class CommandNotFoundException extends CommandException
+{
+	/**
+	 * CommandException constructor
+	 *
+	 * @param message exception message
+	 */
+	public CommandNotFoundException(String message)
+	{
+		super(message);
+	}
+}
---
+++ common/src/main/java/com/megateam/common/exception/impl/command/DefaultExecutorException.java
@@ -0,0 +1,19 @@
+package com.megateam.common.exception.impl.command;
+
+import com.megateam.common.exception.CommandException;
+
+/**
+ * This exception is thrown during execution if default execute(...) method is being used
+ */
+public class DefaultExecutorException extends CommandException
+{
+	/**
+	 * CommandException constructor
+	 *
+	 * @param message exception message
+	 */
+	public DefaultExecutorException(String message)
+	{
+		super(message);
+	}
+}
