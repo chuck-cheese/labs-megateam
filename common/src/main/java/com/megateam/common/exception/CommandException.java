06437858a0cc332ad607b468484a2bccce74550f
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 01:39:05 +0300

    implemented group of command execution exceptions

---
+++ common/src/main/java/com/megateam/common/exception/CommandException.java
@@ -0,0 +1,17 @@
+package com.megateam.common.exception;
+
+/**
+ * This group of exceptions is thrown during command interaction
+ */
+public abstract class CommandException extends Exception
+{
+	/**
+	 * CommandException constructor
+	 *
+	 * @param message exception message
+	 */
+	public CommandException(String message)
+	{
+		super(message);
+	}
+}
