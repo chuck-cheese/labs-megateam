8f35c868cc8da5c8ac7180e8f90a202c3bcd451a
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-04 19:36:58 +0300

    implemented a new group of exceptions for the database

---
+++ common/src/main/java/com/megateam/common/exception/DatabaseException.java
@@ -0,0 +1,17 @@
+package com.megateam.common.exception;
+
+/**
+ * This group of exceptions is thrown if something went wrong during the database operations
+ */
+public abstract class DatabaseException extends Exception
+{
+	/**
+	 * DatabaseException constructor
+	 *
+	 * @param message message for the exception
+	 */
+	public DatabaseException(String message)
+	{
+		super(message);
+	}
+}
