0a7e565e1d7e9a6953e6d73296f901d2bedb3fb1
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 01:38:23 +0300

    implemented command execution service abstraction

---
+++ common/src/main/java/com/megateam/common/execution/ExecutionService.java
@@ -0,0 +1,37 @@
+package com.megateam.common.execution;
+
+import com.megateam.common.command.Command;
+import com.megateam.common.exception.CommandException;
+import com.megateam.common.exception.impl.command.DefaultExecutorException;
+
+import java.util.List;
+
+/**
+ * Execution services abstraction
+ */
+public interface ExecutionService
+{
+	/**
+	 * This method is an abstraction for single command execution method
+	 *
+	 * @param command command, that should be executed
+	 * @return command execution status
+	 * @throws DefaultExecutorException if something went wrong during command execution
+	 */
+	default boolean execute(Command command) throws CommandException
+	{
+		throw new DefaultExecutorException("You are now able to use default command execution method");
+	}
+
+	/**
+	 * This method is an abstraction for commands script execution method
+	 *
+	 * @param script script to be executed
+	 * @return script execution status
+	 * @throws DefaultExecutorException if something went wrong during command script execution
+	 */
+	default boolean execute(List<Command> script) throws CommandException
+	{
+		throw new DefaultExecutorException("You are now able to use default script execution method");
+	}
+}
