b3dafb170c494f9b4bd4ecf3554b337a4ad4d939
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 01:05:06 +0300

    implemented dao abstraction

---
+++ common/src/main/java/com/megateam/common/dao/Dao.java
@@ -0,0 +1,114 @@
+package com.megateam.common.dao;
+
+import com.megateam.common.data.util.TicketType;
+import com.megateam.common.exception.impl.database.ElementIdAlreadyExistsException;
+import com.megateam.common.exception.impl.database.ElementNotFoundException;
+import com.megateam.common.exception.impl.database.UnableToSaveDatabaseException;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * An abstraction for application data access object
+ * @param <T> type, that dao will work with
+ */
+public interface Dao<T>
+{
+	/**
+	 * This is an abstraction for method retrieves creation date of the database
+	 *
+	 * @return database creation date
+	 */
+	LocalDateTime getCreationDate();
+
+	/**
+	 * This is an abstraction for method that retrieves all elements from the database
+	 *
+	 * @return list of stored elements
+	 */
+	List<T> findAll();
+
+	/**
+	 * This is an abstraction for method that is retrieving stored element in the database by id
+	 *
+	 * @param id stored element id
+	 * @return element
+	 * @throws ElementNotFoundException if the specified id does not exist in the database
+	 */
+	T findElementById(@NonNull Integer id) throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that provides an ability to add new element into the database
+	 *
+	 * @param item the new element which should be added into the database
+	 */
+	void add(@NonNull T item) throws ElementIdAlreadyExistsException;
+
+	/**
+	 * This is an abstraction for method that provides an ability to update the existing element by id
+	 *
+	 * @param id existing element id
+	 * @param item element that the existing element will be updated with
+	 * @throws ElementNotFoundException if the specified id does not exist in the database
+	 */
+	void update(@NonNull Integer id, @NonNull T item) throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that removes an element from the database by id
+	 *
+	 * @param id element id
+	 * @throws ElementNotFoundException if the specified id does not exist in the database
+	 */
+	void remove(@NonNull Integer id) throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that clears the database
+	 */
+	void clear();
+
+	/**
+	 * This is an abstraction for method that saves the collection to a file
+	 *
+	 * @throws UnableToSaveDatabaseException if something went wrong during the database saving
+	 */
+	void save() throws UnableToSaveDatabaseException;
+
+	/**
+	 * This is an abstraction for method that removes the first element from the database
+	 *
+	 * @throws ElementNotFoundException if required element not found
+	 */
+	void removeFirst() throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that removes the last element from the database
+	 *
+	 * @throws ElementNotFoundException if required element not found
+	 */
+	void removeLast() throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that removes element which is lower than specified one
+	 *
+	 * @param item an element for the comparison
+	 * @throws ElementNotFoundException if required element not found
+	 */
+	void removeLower(@NonNull T item) throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that removes all the elements with the refundable status specified
+	 *
+	 * @param refundable refundable status of removable elements
+	 * @throws ElementNotFoundException if it's nothing to remove
+	 */
+	void removeAnyByRefundable(@NonNull Boolean refundable) throws ElementNotFoundException;
+
+	/**
+	 * This is an abstraction for method that retrieves all the elements with type less than specified
+	 *
+	 * @param type type to filter
+	 * @return list of filtered elements
+	 */
+	List<T> findLessThanType(@NonNull TicketType type);
+}
