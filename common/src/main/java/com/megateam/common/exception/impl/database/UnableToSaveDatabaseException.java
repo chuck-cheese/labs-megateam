d887b25f5a638bee90c42891b40c4678858526ec
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-04 19:49:45 +0300

    implemented an exception wich is thrown is something went wrong during the database saving

---
+++ common/src/main/java/com/megateam/common/exception/impl/database/UnableToSaveDatabaseException.java
@@ -0,0 +1,19 @@
+package com.megateam.common.exception.impl.database;
+
+import com.megateam.common.exception.DatabaseException;
+
+/**
+ * This exception is thrown during the database saving operation if something went wrong
+ */
+public class UnableToSaveDatabaseException extends DatabaseException
+{
+	/**
+	 * DatabaseException constructor
+	 *
+	 * @param message message for the exception
+	 */
+	public UnableToSaveDatabaseException(String message)
+	{
+		super(message);
+	}
+}
