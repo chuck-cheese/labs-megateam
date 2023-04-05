2bf202186e480e9ebdea91530f3fb624400c85bd
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 15:43:17 +0300

    implemented a venue validation utility

---
+++ common/src/main/java/com/megateam/common/data/validation/VenueValidator.java
@@ -0,0 +1,67 @@
+package com.megateam.common.data.validation;
+
+import com.megateam.common.data.Venue;
+import com.megateam.common.exception.ValidationException;
+import com.megateam.common.exception.impl.validation.BoundValidationException;
+import com.megateam.common.exception.impl.validation.CannotBeEmptyException;
+import com.megateam.common.exception.impl.validation.NullValidationException;
+
+public class VenueValidator
+{
+//	TODO: implement venue validator (static)
+
+	/**
+	 * Method validates venue id
+	 *
+	 * @param id venue id under validation
+	 * @throws BoundValidationException if venue id is less than or equals to zero
+	 */
+	public static void validateVenueId(long id) throws BoundValidationException
+	{
+		if (id <= 0) throw new BoundValidationException("Venue id should be greater than zero");
+	}
+
+	/**
+	 * Validates a venue name for being empty
+	 *
+	 * @param name venue name
+	 * @throws CannotBeEmptyException if venue name is empty
+	 * @throws NullValidationException if venue name is not specified
+	 */
+	public static void validateVenueName(String name) throws ValidationException
+	{
+		if (name == null) throw new NullValidationException("Venue name should be specified");
+
+		if ("".equals(name)) throw new CannotBeEmptyException("Venue name cannot be empty");
+	}
+
+	/**
+	 * Validates a venue capacity for being non-null and greater than zero
+	 *
+	 * @param capacity venue capacity
+	 * @throws NullValidationException if venue capacity is null
+	 * @throws BoundValidationException if venue capacity is less than or equals to zero
+	 */
+	public static void validateVenueCapacity(Integer capacity) throws NullValidationException, BoundValidationException
+	{
+		if (capacity == null) throw new NullValidationException("Venue capacity cannot be null");
+
+		if (capacity <= 0) throw new BoundValidationException("Venue capacity should be greater than zero");
+	}
+
+
+	/**
+	 * Validates an existing venue instance
+	 *
+	 * @param venue venue instance
+	 * @throws BoundValidationException if some fields are out of bound
+	 * @throws NullValidationException if some non-null fields are null
+	 * @throws CannotBeEmptyException if some non-empty fields are empty
+	 */
+	public static void validateVenue(Venue venue) throws ValidationException
+	{
+		validateVenueId(venue.getId());
+		validateVenueName(venue.getName());
+		validateVenueCapacity(venue.getCapacity());
+	}
+}
