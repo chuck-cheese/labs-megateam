9442d9c04f222c699d3bfa0321eaa75c521a4d05
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-05 17:42:35 +0300

    implemented incorrect parsing arg exception

---
+++ common/src/main/java/com/megateam/common/exception/impl/parsing/InaproppriateParsingTypeException.java
@@ -0,0 +1,19 @@
+package com.megateam.common.exception.impl.parsing;
+
+import com.megateam.common.exception.ParsingException;
+
+/**
+ * This exception is thrown if something went wrong during types parsing
+ */
+public class InaproppriateParsingTypeException extends ParsingException
+{
+	/**
+	 * InaproppriateParsingException constructor
+	 *
+	 * @param message exception message
+	 */
+	public InaproppriateParsingTypeException(String message)
+	{
+		super(message);
+	}
+}
