10edbdfa498cd5a376aa5385e9e124ba75966a31
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-12 22:38:28 +0300

    implemented an abstraction for command

---
+++ common/src/main/java/com/megateam/common/command/Command.java
@@ -0,0 +1,78 @@
+package com.megateam.common.command;
+
+import com.megateam.common.dao.Dao;
+import com.megateam.common.data.Ticket;
+import com.megateam.common.exception.CommandException;
+import com.megateam.common.exception.DatabaseException;
+import com.megateam.common.execution.ExecutionService;
+import com.megateam.common.resolving.ResolvingService;
+import com.megateam.common.util.FileManipulationService;
+import com.megateam.common.util.Printer;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * Abstraction for all commands
+ */
+@RequiredArgsConstructor
+public abstract class Command
+{
+	/**
+	 * List of command arguments
+	 */
+	protected final List<String> arguments;
+
+	/**
+	 * Command printer instance
+	 */
+	protected final Printer printer;
+
+	/**
+	 * Command requires collection element status
+	 */
+	@NonNull
+	@Getter
+	protected Boolean RequiresElement;
+
+	/**
+	 * Ticket element is stored there if the command requires it
+	 */
+	@Setter
+	protected Object additionalArgument;
+
+	/**
+	 * Dao for command
+	 */
+	@Setter
+	protected Dao<Ticket> dao;
+
+	/**
+	 * File manipulation service
+	 */
+	@Setter
+	protected FileManipulationService fms;
+
+	/**
+	 * Command execution service (mainly user for script execution)
+	 */
+	@Setter
+	protected ExecutionService executionService;
+
+	/**
+	 * Resolving service (mainly user for script commands resolving)
+	 */
+	@Setter
+	protected ResolvingService resolvingService;
+
+	/**
+	 * This method is an abstraction for command execution method
+	 * @return boolean status of command execution
+	 * @throws CommandException if something went wrong during the command operations
+	 * @throws DatabaseException if something went wrong during the database operations
+	 */
+	public abstract boolean execute() throws CommandException, DatabaseException;
+}
