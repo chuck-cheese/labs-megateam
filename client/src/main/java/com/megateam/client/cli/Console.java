188995ccd2585d444f16d73c7c6f8d62e72424fc
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-12 23:28:14 +0300

    implemented user application environment

---
+++ client/src/main/java/com/megateam/client/cli/Console.java
@@ -0,0 +1,66 @@
+package com.megateam.client.cli;
+
+import com.megateam.common.command.Command;
+import com.megateam.common.execution.ExecutionService;
+import com.megateam.common.resolving.ResolvingService;
+import com.megateam.common.util.Printer;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Scanner;
+
+/**
+ * Main application interface
+ */
+@RequiredArgsConstructor
+public class Console
+{
+	/**
+	 * Scanner instance
+	 */
+	private final Scanner scanner;
+
+	/**
+	 * Printer instance
+	 */
+	private final Printer printer;
+
+	/**
+	 * ResolvingService instance
+	 */
+	private final ResolvingService resolvingService;
+
+	/**
+	 * ExecutionService instance
+	 */
+	private final ExecutionService executionService;
+
+	/**
+	 * Application initialisation method
+	 */
+	public void run()
+	{
+
+		while (true)
+		{
+			printer.print("Enter command: ");
+
+			if (!scanner.hasNextLine())
+			{
+				printer.println("Successfully interrupted...");
+				System.exit(0);
+			}
+
+			String line = scanner.nextLine();
+
+			try
+			{
+				Command command = resolvingService.resolve(line);
+				executionService.execute(command);
+			}
+			catch (Exception e)
+			{
+				printer.println(e.getMessage());
+			}
+		}
+	}
+}
