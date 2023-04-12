6b6db9038928335fa632449a943d35a0303964e4
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-12 22:43:13 +0300

    implemented show command

---
+++ common/src/main/java/com/megateam/common/command/impl/ShowCommand.java
@@ -0,0 +1,35 @@
+package com.megateam.common.command.impl;
+
+import com.megateam.common.command.Command;
+import com.megateam.common.util.Printer;
+
+import java.util.List;
+
+/**
+ * Command shows a list of stored elements
+ */
+public class ShowCommand extends Command
+{
+	/**
+	 * ShowCommand constructor
+	 *
+	 * @param arguments command arguments
+	 * @param printer command printer
+	 */
+	public ShowCommand(List<String> arguments, Printer printer)
+	{
+		super(arguments, printer, false);
+	}
+
+	/**
+	 * This method is an abstraction for command execution method
+	 *
+	 * @return boolean status of command execution
+	 */
+	@Override
+	public boolean execute()
+	{
+		printer.printList(dao.findAll());
+		return true;
+	}
+}
