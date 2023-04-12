d4a10ea907cd9c4240901ea23b9ae3c04675873a
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-12 23:10:28 +0300

    added new exception types to the method signature

--- common/src/main/java/com/megateam/common/resolving/ResolvingService.java
+++ common/src/main/java/com/megateam/common/resolving/ResolvingService.java
@@ -2,6 +2,8 @@ package com.megateam.common.resolving;
 
 import com.megateam.common.command.Command;
 import com.megateam.common.exception.CommandException;
+import com.megateam.common.exception.ParsingException;
+import com.megateam.common.exception.ValidationException;
 import com.megateam.common.exception.impl.command.DefaultResolverException;
 
 import java.io.File;
@@ -19,7 +21,7 @@ public interface ResolvingService
 	 * @return command instance
 	 * @throws CommandException if something went wrong during the command resolving
 	 */
-	default Command resolve(String commandLine) throws CommandException
+	default Command resolve(String commandLine) throws CommandException, ValidationException, ParsingException
 	{
 		throw new DefaultResolverException("You are now able to use default command resolving method");
 	}
@@ -31,7 +33,7 @@ public interface ResolvingService
 	 * @return list of command prepared for execution
 	 * @throws CommandException if something went wrong during the command resolving
 	 */
-	default List<Command> resolve(File script) throws CommandException
+	default List<Command> resolve(File script) throws CommandException, ValidationException
 	{
 		throw new DefaultResolverException("You are now able to use default command script resolving method");
 	}
