d481555d44c663d084137c9f2188929a3513bfc7
Vladislav Andreev <andreevvlad443@gmail.com>
2023-04-14 19:21:01 +0300

    implemented env helper

---
+++ server/src/main/java/com/megateam/server/util/EnvHelper.java
@@ -0,0 +1,17 @@
+package com.megateam.server.util;
+
+/**
+ * Utility class for env operations
+ */
+public class EnvHelper
+{
+	/**
+	 * Method for retrieving db saving file from the env variable SAVING_FILE
+	 *
+	 * @return db saving file location
+	 */
+	public static String retrieveSavingFileLocation()
+	{
+		return System.getenv("SAVING_FILE");
+	}
+}
